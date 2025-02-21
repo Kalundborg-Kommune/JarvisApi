13b3b8ef9ab25a854d643b290635a28ac126d3b5
Tobias Rosenvinge <torh@kalundborg.dk>
2025-02-21 22:50:57 +0100

    add: logging

--- src/main.py
+++ src/main.py
@@ -1,15 +1,20 @@
 from fastapi import FastAPI, HTTPException
 from module.chat_ai import ChatAI
 from model.image_analyzer import ImageAnalyzer
+import logging
+
+logger = logging.getLogger(__name__)
 
 app = FastAPI()
 
 @app.post("/analyze-image")
 async def analyze_image_async(data: ImageAnalyzer):
-    chat = ChatAI()
     try:
+        chat = ChatAI()
         response = chat.analyze_image(data)
         return response
-    except Exception as e:
-        print(e)
-        raise HTTPException(status_code=404, detail="Request could not be made")
+    except ValueError as error:
+        raise HTTPException(status_code=401, detail=str(error))
+    except Exception as ex:
+        logger.exception(ex)
+        raise HTTPException(status_code=503, detail="An error has occurred while handling the request.")
